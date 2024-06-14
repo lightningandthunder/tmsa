b26e9738234eca80a743e405b2ef297cbcc4c10e
Mike <mikepverducci@gmail.com>
2024-06-14 01:38:10 -0700

    MV uniwheel unit tests

--- test/utils.py
+++ test/utils.py
@@ -1,5 +1,5 @@
-def assert_line_contains(line: str, text: str, starts_at: int = 0):
-    if starts_at == 0:
+def assert_line_contains(line: str, text: str, starts_at: int = 0, any_position: bool = False):
+    if any_position:
         if text in line:
             return
         else:
@@ -11,7 +11,11 @@ def assert_line_contains(line: str, text: str, starts_at: int = 0):
     if len(text) > len(line[starts_at:]):
         raise AssertionError(f"Line of length {len(line[starts_at:])} is shorter than text of length {len(text)}")
 
-    for index, char in enumerate(line[starts_at:]):
+    for index, char in enumerate(text):
         if not text[index] == char:
             raise AssertionError(f"Line {line[starts_at:]}\ndoes not match\n'{text}'\nat index {index}")
 
+
+def print_file_output(file: str):
+    for index, line in enumerate(file.split('\n')):
+        print(f'{index: <3}: {line}')
