c97ecba26c5a6464fe6d8ff65b8bf3bff8c4ad84
Mike <mikepverducci@gmail.com>
2024-06-28 01:51:02 -0700

    Add back planetary constants

--- src/constants.py
+++ src/constants.py
@@ -52,3 +52,133 @@ MONTHS = [
     'Nov',
     'Dec',
 ]
+
+PLANETS = {
+    "Moon": {
+        "short_name": "Mo",
+        "long_name": "Moon",
+        "number": 0,
+    },
+    "Sun": {
+        "short_name": "Su",
+        "long_name": "Sun",
+        "number": 1,
+    },
+    "Mercury": {
+        "short_name": "Me",
+        "long_name": "Mercury",
+        "number": 2,
+    },
+    "Venus": {
+        "short_name": "Ve",
+        "long_name": "Venus",
+        "number": 3,
+    },
+    "Mars": {
+        "short_name": "Ma",
+        "long_name": "Mars",
+        "number": 4,
+    },
+    "Jupiter": {
+        "short_name": "Ju",
+        "long_name": "Jupiter",
+        "number": 5,
+    },
+    "Saturn": {
+        "short_name": "Sa",
+        "long_name": "Saturn",
+        "number": 6,
+    },
+    "Uranus": {
+        "short_name": "Ur",
+        "long_name": "Uranus",
+        "number": 7,
+    },
+    "Neptune": {
+        "short_name": "Ne",
+        "long_name": "Neptune",
+        "number": 8,
+    },
+    "Pluto": {
+        "short_name": "Pl",
+        "long_name": "Pluto",
+        "number": 9,
+    },
+    "Mean Node": {
+        "short_name": "MN",
+        "long_name": "Mean Node",
+        "number": 10,
+    },
+    "True Node": {
+        "short_name": "TN",
+        "long_name": "True Node",
+        "number": 11,
+    },
+    "Eris": {
+        "short_name": "Er",
+        "long_name": "Eris",
+        "number": 146199,
+    },
+
+    # Exeprimental bodies
+    "Sedna": {
+        "short_name": "Se",
+        "long_name": "Sedna",
+        "number": 100377,
+    },
+    "Chiron": {
+        "short_name": "Ch",
+        "long_name": "Chiron",
+        "number": 15,
+    },
+    "Ceres": {
+        "short_name": "Ce",
+        "long_name": "Ceres",
+        "number": 17,
+    },
+    "Pallas": {
+        "short_name": "Pa",
+        "long_name": "Pallas",
+        "number": 18,
+    },
+    "Juno": {
+        "short_name": "Jn",
+        "long_name": "Juno",
+        "number": 19,
+    },
+    "Vesta": {
+        "short_name": "Vs",
+        "long_name": "Vesta",
+        "number": 20,
+    },
+    "Orcus": {
+        "short_name": "Or",
+        "long_name": "Orcus",
+        "number": 100482,
+    },
+    "Haumea": {
+        "short_name": "Ha",
+        "long_name": "Haumea",
+        "number": 146108,
+    },
+    "Makemake": {
+        "short_name": "Mk",
+        "long_name": "Makemake",
+        "number": 146472,
+    },
+    "Gonggong": {
+        "short_name": "Go",
+        "long_name": "Gonggong",
+        "number": 235088,
+    },
+    "Quaoar": {
+        "short_name": "Qu",
+        "long_name": "Quaoar",
+        "number": 60000,
+    },
+    "Salacia": {
+        "short_name": "Sl",
+        "long_name": "Salacia",
+        "number": 130347,
+    },
+}
